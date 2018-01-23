bf92d0bd09076921d2fd08ee43304abd36c28c76
Chris Braden <crbraden@uw.edu>
2018-01-23 14:19:32 -0800

    Adding solution for PS2-7

--- Braden_PS2-4.py
+++ Braden_PS2-4.py
@@ -5,21 +5,11 @@
 
 import sys
 
-user_file = open(sys.argv[1], "r")
+user_file = open(sys.argv[1], "r") # read user supplied file name and open file
 
+lines = user_file.readlines() # obtain list  from user file
+rev_lines = reversed(lines) # make a list of lines in reverse
 
-#lines = list(user_file.readlines())
-#rev_lines = list(reversed(lines))
+print ''.join(rev_lines) # print out the lines as a string
 
-lines = user_file.readlines()
-rev_lines = reversed(lines)
-
-
-print 'lines: ', ''.join(lines)
-print 'rev_lines: ', ''.join(rev_lines)
-
-user_file.close()
-
-
-
-
+user_file.close() # close open file
