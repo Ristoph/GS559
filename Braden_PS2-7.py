7c5389771fb5f2ad565080b1748a9175e4def1bb
Chris Braden <crbraden@uw.edu>
2018-01-23 15:06:42 -0800

    final answer for PS2-7

--- Braden_PS2-7.py
+++ Braden_PS2-7.py
@@ -7,12 +7,18 @@ import sys
 
 file1 = open(sys.argv[1], "r") # read user supplied file names and open files
 file2 = open(sys.argv[2], "r")
-
 file_name = sys.argv[3] # read user supplied name for combined file
+
 combined_file = open(file_name, "w") # create a file with specified name
 
 data_file1 = file1.readlines() # read lines of user supplied files
 data_file2 = file2.readlines()
+
+if (data_file1[len(data_file1)-2:len(data_file1)-1].count('\n') == 0): # adding carriage returns to first file if absent b/c ocd
+    data_file1.append('\r\n')
+    data_file1.append('\r\n')
+elif (data_file1[len(data_file1)-2:len(data_file1)-1].count('\n') == 1):
+    data_file1.append('\r\n')
 
 data_file1 = ''.join(data_file1) # convert file lists to strings
 data_file2 = ''.join(data_file2)
