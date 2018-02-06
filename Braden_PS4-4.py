b84731a941f81a302309f4b646bdbaeb9c35e1b0
Ristoph <christopherbraden@gmail.com>
2018-02-05 19:07:13 -0800

    PS4-4 done

--- Braden_PS4-4.py
+++ Braden_PS4-4.py
@@ -1,6 +1,15 @@
 ### Chris Braden PS4 question 4
 ##
-## GIVEN: file containing a single sequence (either DNA or protein) and an integer (k)
-## RETURN: all k-mers occuring at least once in the sequence, listed in alphabetical order with the count of how
-##         many times they occur
+## GIVEN: a blastn output text like example_blastnout.txt
+## RETURN: each alignment score and E-value, one per line
 
+import sys
+
+blastn_file = open(sys.argv[1], 'r')
+
+for line in blastn_file:
+    if 'Score' in line:
+        goods = line.split()
+        print 'Score', str(goods[2]), 'bits,', 'E-value', str(goods[7])
+
+blastn_file.close()
