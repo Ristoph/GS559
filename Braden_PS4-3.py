83dc3b27b5ce0f5ad59534e9b70dd00dbc5a031a
Chris Braden <crbraden@uw.edu>
2018-02-06 09:02:55 -0800

    PS4-3 almost done

--- Braden_PS4-3.py
+++ Braden_PS4-3.py
@@ -10,7 +10,11 @@ seq_file = open(sys.argv[1], 'r')#I need to open the indicated file
 kmer_len = int(sys.argv[2])#I need to store the length of the kmer
 
 seq = list(str(seq_file.read())) # read in sequence file to list of bases and new line characters
-seq.remove('\n') # take out new line characters
+seq = list(val for val in seq if val != '\n')
+
+# seq.remove('\n') # take out new line characters
+
+# print seq[:280]
 
 kmer_lib = {} # make a library to store kmers
 seq_len = len(seq) # measure library length
@@ -20,11 +24,13 @@ while rem_seq >= kmer_len: # so long as there are enough bases to make a kmer
     start = seq_len - rem_seq # window start
     stop = start + kmer_len # window end
     kmer_now = (''.join(seq[start:stop])).upper() # make a string of current kmer in window
-    print str(kmer_lib[kmer_now])
-    # if kmer_now in kmer_lib.keys(): # if we've seen this kmer before
-    #     kmer_lib[kmer_now] = kmer_lib[kmer_now] + 1 # increment the count
-    # else: # if we haven't seen it
-    #     kmer_lib[kmer_now] = 1 # add current kmer to the library
+    # if 'N' in kmer_now: # include if removal of N containing kmers is desired
+    #     rem_seq -= 1
+    #     continue
+    if kmer_now in kmer_lib.keys(): # if we've seen this kmer before
+        kmer_lib[kmer_now] = kmer_lib[kmer_now] + 1 # increment the count
+    else: # if we haven't seen it
+        kmer_lib[kmer_now] = 1 # add current kmer to the library
     rem_seq -= 1 # reduce the count of remaining bases
 
 for val in kmer_lib.keys(): # print all entries in the kmer library and their counts in a stupid order
