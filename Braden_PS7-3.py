a028d834e33bb5afc75ea5f4b637cc08f8a7409f
Chris Braden <crbraden@uw.edu>
2018-03-07 13:42:27 -0800

    PS7-3

--- Braden_PS7-3.py
+++ Braden_PS7-3.py
@@ -3,3 +3,26 @@
 # space. You can use the function math.pow(x,y) that returns x raised to the power y
 # and the function math.sqrt(x) that returns the square root of x
 
+import math
+import sys
+
+def CalcDistance(coord1, coord2):   #calculates the euclidean distance between two n-dimensional points
+    squares_sum = 0
+    for dim in range(len(coord1)):
+        square_diff = math.pow((int(coord1[dim])-int(coord2[dim])), 2)
+        squares_sum += square_diff
+    return math.sqrt(squares_sum)
+
+
+point1 = sys.argv[1]            # takes coordinate inputs as comma separated elements without whitespace
+point1 = point1.split(',')
+
+point2 = sys.argv[2]
+point2 = point2.split(',')
+
+
+euclid_dist = CalcDistance(point1, point2)
+
+print point1, "and", point2, "are", euclid_dist, "apart"
+
+
